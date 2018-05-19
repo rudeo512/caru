ed8b5db2bff6a507f2dd3b36d14fc3fb56eac9d0
조경대 <kyungdae.cho@navercorp.com>
2018-05-19 09:31:23 +0900

    수정

--- web/src/main/java/com/caru/web/controller/WebController.java
+++ web/src/main/java/com/caru/web/controller/WebController.java
@@ -1,8 +1,12 @@
 package com.caru.web.controller;
 
+import java.util.Arrays;
+
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.caru.core.service.PostsService;
 import lombok.AllArgsConstructor;
@@ -18,10 +22,19 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class WebController {
 	private PostsService postsService;
+	private Environment env;
 
 	@GetMapping("/")
 	public String main(Model model) {
 		model.addAttribute("posts", postsService.findAllDesc());
 		return "main";
+	}
+
+	@ResponseBody
+	@GetMapping("/profile")
+	public String getProfile() {
+		return Arrays.stream(env.getActiveProfiles())
+			.findFirst()
+			.orElse("");
 	}
 }
