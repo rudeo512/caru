dedf7e4cd3737a19ed76338e754afdf22ee8132e
kyungdae.cho <kyungdae.cho@navercorp.com>
2018-06-20 16:48:44 +0900

    oauth 적용

--- web/src/main/java/com/caru/config/SecurityConfig.java
+++ web/src/main/java/com/caru/config/SecurityConfig.java
@@ -16,12 +16,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-			.antMatchers("/oauth_login")
+			.antMatchers("/login")
 			.permitAll()
 			.anyRequest()
 			.authenticated()
 			.and()
 			.oauth2Login()
-			.loginPage("/oauth_login");
+			.loginPage("/login")
+			.defaultSuccessUrl("/signup", true);
+
 	}
 }
--- web/src/main/java/com/caru/web/controller/LoginController.java
+++ web/src/main/java/com/caru/web/controller/LoginController.java
@@ -3,13 +3,22 @@ package com.caru.web.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ResolvableType;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * LoginController
@@ -28,7 +37,10 @@ public class LoginController {
 	@Autowired
 	private ClientRegistrationRepository clientRegistrationRepository;
 
-	@GetMapping("/oauth_login")
+	@Autowired
+	private OAuth2AuthorizedClientService authorizedClientService;
+
+	@GetMapping("login")
 	public String getLoginPage(Model model) {
 		Iterable<ClientRegistration> clientRegistrations = null;
 		ResolvableType type = ResolvableType.forInstance(clientRegistrationRepository).as(Iterable.class);
@@ -42,6 +54,28 @@ public class LoginController {
 				authorizationRequestBaseUri + "/" + registration.getRegistrationId()));
 		model.addAttribute("urls", oauth2AuthenticationUrls);
 
-		return "oauth_login";
+		return "login";
+	}
+
+	@GetMapping("/signup")
+	public String socialSignup(Model model, OAuth2AuthenticationToken authentication) {
+		OAuth2AuthorizedClient client = authorizedClientService.loadAuthorizedClient(
+			authentication.getAuthorizedClientRegistrationId(),
+			authentication.getName());
+
+		String userInfoEndpointUri = client.getClientRegistration().getProviderDetails().getUserInfoEndpoint().getUri();
+
+		if (!StringUtils.isEmpty(userInfoEndpointUri)) {
+			RestTemplate restTemplate = new RestTemplate();
+			HttpHeaders headers = new HttpHeaders();
+			headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + client.getAccessToken().getTokenValue());
+			HttpEntity entity = new HttpEntity("", headers);
+
+			ResponseEntity<Map> response = restTemplate.exchange(userInfoEndpointUri, HttpMethod.GET, entity, Map.class);
+			Map userAttributes = response.getBody();
+			model.addAttribute("name", userAttributes.get("name"));
+		}
+
+		return "signup";
 	}
 }
